0c3f856b7d9ac5aa85fcc24d9fceaa2b0a4cfd44
Yuriy Shandor <yuriyshandor@gmail.com>
2017-11-27 17:36:30 +0200

    Add Articles

--- js/script.js
+++ js/script.js
@@ -13,16 +13,46 @@ $(document).ready(function() {
     $('.nav-bar').slideToggle('0.5s');
   });
 
-  var products = ['all', 'print-design', 'animation', 'art', 'web-design', 'photography', 'video'];
-
-  $('.works-categories__category_all').addClass('works-categories__category_active');
+  var products = ['people', 'bussines', 'education', 'life-style'];
 
   products.forEach(function(item) {
-    $(`.works-categories__category_${item}`).click(function(){
-      $('.works-categories__category').removeClass('works-categories__category_active');
-      $(this).addClass('works-categories__category_active');
-      $('.work__all').addClass('work-hidden');
-      $(`.work__${item}`).removeClass('work-hidden');
+    $(`.article-category__${item}`).click(function(){
+      $('.article-category').removeClass('article-category_active');
+      $(this).addClass('article-category_active');
+      $('.article').addClass('article_hidden');
+      $(`.article__${item}`).removeClass('article_hidden');
     });
   });
+
+  // var articleAmount = $(".article").size();
+  //   x=6;
+  //   $('.article:lt('+x+')').show();
+  //   $('#loadMore').click(function () {
+  //       x= (x+3 <= articleAmount) ? x+3 : articleAmount;
+  //       $('.article:lt('+x+')').show();
+  //   });
+
+  // $(".article").slice(0, 4).show();
+  //   $("#loadMore").on('click', function (e) {
+  //       e.preventDefault();
+  //       $(".article:hidden").slice(0, 4).slideDown();
+  //       if ($(".article:hidden").length == 0) {
+  //           $("#load").fadeOut('slow');
+  //       }
+  //       $('html,body').animate({
+  //           scrollTop: $(this).offset().top
+  //       }, 1500);
+  //   });
+
+  size_li = $(".article").size();
+    x=3;
+    $('.article:lt('+x+')').show();
+    $('#loadMore').click(function () {
+        x= (x+5 <= size_li) ? x+5 : size_li;
+        $('.article:lt('+x+')').show();
+         $('#showLess').show();
+        if(x == size_li){
+            $('#loadMore').hide();
+        }
+    });
 });
