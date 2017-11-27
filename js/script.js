2d3aab670bd8418deae48721dd16000457890040
Yuriy Shandor <yuriyshandor@gmail.com>
2017-11-27 21:13:19 +0200

    Add Mod_01

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  var $menu = $(".header-nav");
   $(window).scroll(function(){
-    if ( $(this).scrollTop() > 100 && $menu.hasClass("header-nav_default") ){
-      $menu.removeClass("header-nav_default").addClass("header-nav_fixed");
-    } else if($(this).scrollTop() <= 100 && $menu.hasClass("header-nav_fixed")) {
-      $menu.removeClass("header-nav_fixed").addClass("header-nav_default");
+    if ($(window).scrollTop()) {
+       $('.header-nav').addClass('header-nav_fixed');
     }
-  });//scroll
-
+    else {
+       $('.header-nav').removeClass('header-nav_fixed');
+    }
+  });
 
   $('.toggler-icon').on('click', function() {
     $('.nav-bar').slideToggle('0.5s');
@@ -15,44 +14,27 @@ $(document).ready(function() {
 
   var products = ['people', 'bussines', 'education', 'life-style'];
 
+  var articleToShow = 9;
+    $('.article').hide().slice(0, articleToShow).show();
+    $('#loadMore').click(function () {
+      articleToShow = articleToShow +3;
+      $('.article').hide().slice(0, articleToShow).show();
+  });
+
+  var toShow = 9;
+
   products.forEach(function(item) {
     $(`.article-category__${item}`).click(function(){
       $('.article-category').removeClass('article-category_active');
       $(this).addClass('article-category_active');
       $('.article').addClass('article_hidden');
       $(`.article__${item}`).removeClass('article_hidden');
+
+      $(`.article__${item}`).hide().slice(0, toShow).show();
+      $('#loadMore').click(function () {
+        toShow = toShow +3;
+        $(`.article__${item}`).hide().slice(0, toShow).show();
+      });
     });
   });
-
-  // var articleAmount = $(".article").size();
-  //   x=6;
-  //   $('.article:lt('+x+')').show();
-  //   $('#loadMore').click(function () {
-  //       x= (x+3 <= articleAmount) ? x+3 : articleAmount;
-  //       $('.article:lt('+x+')').show();
-  //   });
-
-  // $(".article").slice(0, 4).show();
-  //   $("#loadMore").on('click', function (e) {
-  //       e.preventDefault();
-  //       $(".article:hidden").slice(0, 4).slideDown();
-  //       if ($(".article:hidden").length == 0) {
-  //           $("#load").fadeOut('slow');
-  //       }
-  //       $('html,body').animate({
-  //           scrollTop: $(this).offset().top
-  //       }, 1500);
-  //   });
-
-  size_li = $(".article").size();
-    x=3;
-    $('.article:lt('+x+')').show();
-    $('#loadMore').click(function () {
-        x= (x+5 <= size_li) ? x+5 : size_li;
-        $('.article:lt('+x+')').show();
-         $('#showLess').show();
-        if(x == size_li){
-            $('#loadMore').hide();
-        }
-    });
 });
